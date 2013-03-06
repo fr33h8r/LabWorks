45c302804eec724db2241ad26350e43acef7610e
pzaloznyi <pzaloznyi@gmail.com>
2013-03-06 16:01:32 +0200

    ended part 1, started pard 2 of labor work

--- Part1Integers/main.cpp
+++ Part1Integers/main.cpp
@@ -1,12 +1,31 @@
 #include "main.h"
-#include <iostream>
-
-using namespace std;
 
 void main()
 {
 	//short constant
-	short number1 = 1;
+	short shortNumber1 = 0x8a;
+	short shortNumber2 = 64;
+	short shortNumber3 = -136;
 
-	cout<<"Hello, world";
+	//int constants
+	int intNumber1 = (int)-5.67; //in runtime will rounded to integer value
+	int intNumber2 = 98;
+	int intNumber3 = 209754;
+	
+	//long constants
+	long longNumber1 = 361;
+	long longNumber2 = (long)78.9;
+	long longNumber3 = -2973738122;
+
+	//byte constants
+	byte byteNumber1 = 588;
+	byte byteNumber2 = 63;
+	byte byteNumber3 = -99;
+
+	//unsigned short constants
+	unsigned short unShortNum1 = 87654;
+	unsigned short unShortNum2 = 90;
+	unsigned short unShortNum3 = 6543;
+
+	_getch();
 }
---
+++ Part1Integers/main.h
@@ -0,0 +1,5 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+typedef unsigned char byte;
